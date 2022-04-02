82d524387c55942c4399727ad2cce68d2b33379c
Carlos Jesus Perez Dugarte <soycarlosdugarte@gmail.com>
2022-04-02 13:12:00 -0300

    Se lanza el browser de cpacitor

--- src/app/components/articule/articule.component.ts
+++ src/app/components/articule/articule.component.ts
@@ -1,6 +1,8 @@
 import { Articule } from './../../interfaces/index.interface';
 import { Component, Input } from '@angular/core';
 
+import { Browser } from '@capacitor/browser';
+
 @Component({
   selector: 'app-articule',
   templateUrl: './articule.component.html',
@@ -11,6 +13,15 @@ export class ArticuleComponent {
   @Input() articule: Articule;
 
 
-  constructor() {}
+  constructor(
+  ) {}
+
+  onClick() {
+    //
+  }
+
+  async openArticle() {
+    await Browser.open({ url: this.articule.url });
+}
 
 }
